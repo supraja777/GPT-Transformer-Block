b591fd68ca5b4781d8cb663bff76bde396e10609
supraja777 <ssupraja777@gmail.com>
2025-12-11 10:33:47 -0500

    Added GELU activation

---
+++ GELU-Activation.py
@@ -0,0 +1,13 @@
+# Instead of RELU we are using another activation function - GELU
+import torch
+import torch.nn as nn
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        )) 
